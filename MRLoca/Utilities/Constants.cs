58411026faae30a5786137963ff71e7804df70cc
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-01-24 12:19:09 +0100

    Correction du carousel en cas d'absence de list Modification de la string connection

--- MRLoca/Utilities/Constants.cs
+++ MRLoca/Utilities/Constants.cs
@@ -8,7 +8,8 @@ namespace MRLoca.Utilities
     public class Constants
     {
         // Chaine de connection BDD
-        public const string StringConnection = @"Server=LOCALHOST\SQLEXPRESS;Database=MRLoca;User Id=sa;Password=123456;";
+        public const string StringConnection = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=MRLoca;Integrated Security=True";
+           // @"Server=LOCALHOST\SQLEXPRESS;Database=MRLoca;User Id=sa;Password=123456;";
         public const string Client = "Client";
         public const string CheminPhoto = "Images/";
     }
--- MRLoca/Utilities/Functions.cs
+++ MRLoca/Utilities/Functions.cs
@@ -43,38 +43,43 @@ namespace MRLoca.Utilities
         public static string GenerationCarousel(List<Hebergement> ListeListeHebergement)
         {
             StringBuilder carousel = new StringBuilder();
-            carousel.AppendLine("<div id = 'carouselExampleIndicators' class='carousel slide' data-ride='carousel'>");
-            carousel.AppendLine("<ol class='carousel-indicators'>");
-            int i = 0;
-            foreach (Hebergement item in ListeListeHebergement)
+            if (ListeListeHebergement != null && ListeListeHebergement.Count()>0)
             {
-                if (i == 0)
+                
+                carousel.AppendLine("<div id = 'carouselExampleIndicators' class='carousel slide' data-ride='carousel'>");
+                carousel.AppendLine("<ol class='carousel-indicators'>");
+                int i = 0;
+                foreach (Hebergement item in ListeListeHebergement)
                 {
-                    carousel.AppendLine("<li data-target='#carouselExampleIndicators' data-slide-to='" + i + "' class='active'></li>");
-                    i++;
+                    if (i == 0)
+                    {
+                        carousel.AppendLine("<li data-target='#carouselExampleIndicators' data-slide-to='" + i + "' class='active'></li>");
+                        i++;
+                    }
+                    else
+                    {
+                        carousel.AppendLine("<li data-target='#carouselExampleIndicators' data-slide-to='" + i + "'></li>");
+                        i++;
+                    }
                 }
-                else
+                carousel.AppendLine("</ol><div class='carousel-inner border shadow my-5 bg-white rounded'>");
+                int j = 0;
+                foreach (Hebergement item in ListeListeHebergement)
                 {
-                    carousel.AppendLine("<li data-target='#carouselExampleIndicators' data-slide-to='" + i + "'></li>");
-                    i++;
+                    if (j == 0)
+                    {
+                        carousel.AppendLine("<div class='carousel-item active'>");
+                    }
+                    else
+                    {
+                        carousel.AppendLine("<div class='carousel-item'>");
+                    }
+                    carousel.AppendLine(" <a href='DetailHebergement.aspx?id=" + item.IdHebergement + @"'><img Class='d-block w-100' src='" + item.Photo + @"' ID='imgPhoto" + j + @"' runat='server' /><div class='carousel-caption d-none d-md-block'><h5>" + item.Nom + "</h5></div></a></div>");
+                    j++;
                 }
+                carousel.AppendLine("< a class='carousel-control-prev' href='#carouselExampleIndicators' role='button' data-slide='prev'><span class='carousel-control-prev-icon' aria-hidden='true'></span><span class='sr-only'>Previous</span></a><a class='carousel-control-next' href='#carouselExampleIndicators' role='button' data-slide='next'><span class='carousel-control-next-icon' aria-hidden='true'></span><span class='sr-only'>Next</span></a></div></div>");
+
             }
-            carousel.AppendLine("</ol><div class='carousel-inner border shadow my-5 bg-white rounded'>");
-            int j = 0;
-            foreach (Hebergement item in ListeListeHebergement)
-            {
-                if (j == 0)
-                {
-                    carousel.AppendLine("<div class='carousel-item active'>");                   
-                }
-                else
-                {
-                    carousel.AppendLine("<div class='carousel-item'>");                    
-                }
-                carousel.AppendLine(" <a href='DetailHebergement.aspx?id=" + item.IdHebergement + @"'><img Class='d-block w-100' src='" + item.Photo + @"' ID='imgPhoto" + j + @"' runat='server' /><div class='carousel-caption d-none d-md-block'><h5>"+item.Nom+"</h5></div></a></div>");
-                j++;
-            }
-            carousel.AppendLine("< a class='carousel-control-prev' href='#carouselExampleIndicators' role='button' data-slide='prev'><span class='carousel-control-prev-icon' aria-hidden='true'></span><span class='sr-only'>Previous</span></a><a class='carousel-control-next' href='#carouselExampleIndicators' role='button' data-slide='next'><span class='carousel-control-next-icon' aria-hidden='true'></span><span class='sr-only'>Next</span></a></div></div>");
             return carousel.ToString();
         }
 
